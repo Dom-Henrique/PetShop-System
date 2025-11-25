93f76e45513d8182cc34832311ad3c9af5134db8
Dom-Henrique <joaohenriqueds24@gmail.com>
2025-11-25 13:18:13 -0300

    New data bases [IN MAINTENANCE]

--- main/models.py
+++ main/models.py
@@ -5,8 +5,32 @@ from flask_login import UserMixin # Serve para identificar a classe como uma cla
 
 class User(UserMixin, db.Model):
     __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True) # Preencher automaticamente
     username = db.Column(db.String, nullable=False, unique=True)
     email = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable = False)
-    type_user = db.Column(db.Integer, nullable=False)
+    #type_user = db.Column(db.Integer, nullable=False)
+    
+class Products(db.Model):
+    __tablename__ = "products"
+    prod_id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String, nullable=False)
+    product_desc = db.Column(db.String, nullable=False)
+    prod_category = db.Column(db.Sting, nullable=False)
+    product_price = db.Column(db.Float, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    
+class Services(db.Model):
+    __tablename__ = "services"
+    serv_id = db.Column(db.Integer, primary_key=True)
+    service_name = db.Column(db.String, nullable=False)
+    service_desc = db.Column(db.String, nullable=False)
+    serv_category = db.Column(db.String, nullable=False)
+    professional = db.Column(db.ForeignKey())
+    service_price = db.Column(db.Integer, nullable=False)
+    
+class Professional(db.Model):
+    __tablename__ = "professionals"
+    prof_id = db.Column(db.Integer, primary_key=True)
+    prof_name = db.Column(db.String, nullable=False)
+    prof_ocupation = db.Column(db.String, nullable=False)
