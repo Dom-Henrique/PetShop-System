9a50b60d841af67889b453b4e48707b933653b27
Dom-Henrique <joaohenriqueds24@gmail.com>
2025-12-26 21:01:27 -0300

    Manutenção das tabelas de Usuários e Profissionais

--- main/models.py
+++ main/models.py
@@ -1,36 +1,36 @@
 # Aqui só importa a classe
-
+from random import randint
 from db import db
 from flask_login import UserMixin # Serve para identificar a classe como uma classe pelo login_manager
 
 class User(UserMixin, db.Model):
     __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True) # Preencher automaticamente
-    username = db.Column(db.String, nullable=False, unique=True)
+    id = db.Column(db.Integer, primary_key=True, unique=True) # Preencher automaticamente
+    username = db.Column(db.String, nullable=False, unique=True, server_default=db.text(f"USER{randint(100000, 999999)}"))
     email = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable = False)
-    #type_user = db.Column(db.Integer, nullable=False)
+    type_user = db.Column(db.Integer, nullable=False, server_default='2')
     
 class Products(db.Model):
     __tablename__ = "products"
-    prod_id = db.Column(db.Integer, primary_key=True)
+    prod_id = db.Column(db.Integer, primary_key=True, unique=True)
     product_name = db.Column(db.String, nullable=False)
     product_desc = db.Column(db.String, nullable=False)
-    prod_category = db.Column(db.Sting, nullable=False)
+    prod_category = db.Column(db.String, nullable=False)
     product_price = db.Column(db.Float, nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
     
 class Services(db.Model):
     __tablename__ = "services"
-    serv_id = db.Column(db.Integer, primary_key=True)
+    serv_id = db.Column(db.Integer, primary_key=True, unique=True)
     service_name = db.Column(db.String, nullable=False)
     service_desc = db.Column(db.String, nullable=False)
     serv_category = db.Column(db.String, nullable=False)
-    professional = db.Column(db.ForeignKey())
+    professional = db.Column(db.ForeignKey('professionals.prof_name'))
     service_price = db.Column(db.Integer, nullable=False)
     
 class Professional(db.Model):
     __tablename__ = "professionals"
-    prof_id = db.Column(db.Integer, primary_key=True)
+    prof_id = db.Column(db.Integer, primary_key=True, unique=True)
     prof_name = db.Column(db.String, nullable=False)
     prof_ocupation = db.Column(db.String, nullable=False)
